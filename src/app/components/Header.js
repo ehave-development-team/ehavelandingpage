4dd437b6c40da6dad6e95f21014a822b73500a54
ehave-development-team <development@ehave.com>
2024-01-31 23:03:43 -0500

    Update to the front page with the message to capture users

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -6,11 +6,11 @@ export default function Header(){
 
         <div className="px-5 py-5 header">
             <div className='mt-14' style={{maxWidth:'700px'}}><p className="text-2xl font-medium mb-8 text-teal-900">
-                ＭＵＬＴ♢Ｒ</p>
+                Ehave</p>
             <p className="lg:text-7xl text-5xl leading-snug  font-bold text-teal-950 mb-8 lg:leading-snug">
-            Efficiency, convenience, and guaranteed time slot availability.</p>
+            Changing The Way You Live Life, Through Data.</p>
             <p className="tracking-wider text-xl mt-14 w-2/3 mb-14 text-teal-950 font-light">
-            Experience seamless scheduling with our user-friendly appointment services, tailored to your convenience and preferences.</p></div>
+            A powerful endeavor of how data and the mathematical model of humanity, transforms every aspect of our lives.</p></div>
         </div>
 
 
