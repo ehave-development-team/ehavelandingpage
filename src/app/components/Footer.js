7d24334a1779fccf781a84966d9682bc97cf6c32
ehave-development-team <development@ehave.com>
2024-01-31 22:54:10 -0500

    Update to footer with ehave inc

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer(){
 
         <div className="bg-teal-950 text-center h-40 text-gray-400 pt-10 md:flex md:flex-row md:justify-around
          md:items-center">
-            <p className="text-3xl mb-6 ">ＭＵＬＴ♢Ｒ</p>
-            <p>© 2023 Ankit Yadav. All rights reserved.</p>
+            <p className="text-3xl mb-6 ">Ehave</p>
+            <p>© 2024 Ehave, Inc. All rights reserved.</p>
 
         </div>
     )
